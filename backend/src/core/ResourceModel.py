74c5ff7a928e44814afb1b75236daaf31f5e03fd
Joseph Benjamin Ilagan <joben.ilagan@gmail.com>
2019-04-28 14:22:00 +0000

    Added core Classes

---
+++ backend/src/core/ResourceModel.py
@@ -0,0 +1,7 @@
+'''
+ResourceModel.py
+- Abstract Class to be used for Resource Model interaction with databases
+'''
+
+class ResourceModel:
+    pass
---
+++ backend/src/core/TestResourceModel.py
@@ -0,0 +1,6 @@
+from ResourceModel import ResourceModel
+
+class TestResourceModel(ResourceModel):
+    
+    def hello(self):
+        print("Hello World")
